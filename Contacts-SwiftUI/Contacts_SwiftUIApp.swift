de57cf5673afeeac58c4c3115da0dbe17421dcdc
Vasily Petuhov <vspetukhov@icloud.com>
2020-10-30 15:49:45 +0300

    Initial Commit

---
+++ Contacts-SwiftUI/Contacts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_SwiftUIApp.swift
+//  Contacts-SwiftUI
+//
+//  Created by Василий Петухов on 30.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
